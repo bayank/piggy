939472173bd84934f0e0759225a8d18803e6403d
bayank <30418939+bayank@users.noreply.github.com>
2020-10-05 21:28:13 -0400

    Create piggy.py

---
+++ piggy.py
@@ -0,0 +1,13 @@
+def pig_latin(text):
+    words = text.split(' ')
+    pig3 = []
+    for x in words:
+        # Remove the 0th character for each string in the xth element in list words
+        pig2 = x.strip(x[0])
+        # Concactenate the resulting string with the 0th character and add "ay" to the end
+        pigay = [pig2 + x[0] + "ay"]
+        # Create a new list by adding the xth element
+        pig3 = pig3 + pigay
+        # Join all the elements in the list into a string separated by a space, and capitalize the first letter
+        pig4 = " ".join(pig3).capitalize()
+    return pig4
